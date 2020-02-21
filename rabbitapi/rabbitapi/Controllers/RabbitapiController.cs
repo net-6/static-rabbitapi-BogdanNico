705515d544b2236b71a2fbcf458d57468a0cda8d
BogdanNico <57284588+BogdanNico@users.noreply.github.com>
2020-02-21 22:04:20 +0200

    Add files via upload

---
+++ rabbitapi/rabbitapi/Controllers/RabbitapiController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using rabbitapi.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace rabbitapi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class RabbitapiController : ControllerBase
+    {
+        private static List<Rabbit> myRabbits = new List<Rabbit>();
+
+        public RabbitapiController ()
+
+        {
+            Rabbit bunny = new Rabbit("Ion", Rabbit.FurColors.Black, Rabbit.EyeColors.Blue, Rabbit.Genders.Male, 5, 1);
+            Rabbit buggsbunny = new Rabbit("Ana", Rabbit.FurColors.Brown, Rabbit.EyeColors.Red, Rabbit.Genders.Female, 6, 2);
+
+            if (!myRabbits.Any())
+            {
+                myRabbits.Add(bunny);
+                myRabbits.Add(buggsbunny);
+            }
+        }
+
+        // GET: api/Rabbitapi
+        [HttpGet]
+        public IEnumerable<Rabbit> Get()
+        {
+            return myRabbits;
+        }
+
+        // GET api/Rabbitapi/2
+        [HttpGet("{id}", Name = "Get")]
+        public Rabbit Get(int id)
+        {
+            Rabbit buggsbunny = new Rabbit("Ana", Rabbit.FurColors.Brown, Rabbit.EyeColors.Red, Rabbit.Genders.Female, 6, 2);
+            return buggsbunny;  
+        }
+
+        // POST api/Rabbitapi
+        [HttpPost]
+        public IActionResult Post([FromBody] Rabbit model)
+        {
+            if (ModelState.IsValid)
+            {
+                //save in 'database'
+                myRabbits.Add(model);
+                return Ok();
+            }
+            return BadRequest();
+        }
+
+        // PUT api/Rabbitapi/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ rabbitapi/rabbitapi/Models/rabbit.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace rabbitapi.Models
+{
+    public class Rabbit
+    {
+        public enum FurColors
+        {
+            White,
+            Brown,
+            Black,
+            Grey
+        }
+        public enum EyeColors
+        {
+            Blue,
+            Black,
+            Red
+        }
+        public enum Genders
+        {
+            Male,
+            Female
+        }
+        
+        [Required]
+        public String Name { get; set; }
+
+        [Required]
+        public FurColors FurColor { get; set; }
+
+        [Required]
+        public EyeColors EyeColor { get; set; }
+
+        [Required]
+        public Genders Gender { get; set; }
+
+        [Required]
+        public int Age { get; set; }
+
+        [Required]
+        public int Id { get; set; }
+
+       
+        public Rabbit(String name, FurColors furColor, EyeColors eyeColor, Genders gender, int age, int id)
+        {
+            Name = name;
+            FurColor = furColor;
+            EyeColor = eyeColor;
+            Gender = gender;
+            Age = age;
+            Id = id;
+        }
+        public void Move()
+        {
+            Console.WriteLine("The rabbit is moving left and right like crazy.");
+        }
+        public void Sleep()
+        {
+            Console.WriteLine("The rabbit is sleeping like a rock.");
+        }
+        public void Eat()
+        {
+            Console.WriteLine("The rabbit is eating some food");
+        }
+     
+
+    }
+}
